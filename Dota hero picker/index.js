9d6f026e99df26d77a8e6441c5474424d7bea988
Hashir Anis <hashir.work@hotmail.com>
2024-12-24 01:24:34 +0500

    Added 3rd project

---
+++ Dota hero picker/index.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("btn");
+const selectedHero = document.querySelector(".selectedHero");
+
+import herodata from './herodata.json' with { type: 'json' };
+  
+// Heroes JSON
+// console.log(herodata.result.heroes);
+
+    const heroId = herodata.result.heroes;
+    const heroName = herodata.result.heroes;
+
+console.log('\n');
+
+function getRandomNumber(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+    // console.log(item)
+    return item;
+
+}
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber(herodata.result.heroes);
+//   console.log(randomNumber);
+  
+  selectedHero.textContent = randomNumber.localized_name;
+});
+
