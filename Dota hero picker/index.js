4e6a03faddb59cd68bcf55c338874cc6a6f1cf91
Hashir Anis <hashir.work@hotmail.com>
2024-12-24 19:21:21 +0500

    Added description; styling fixes

--- Dota hero picker/index.js
+++ Dota hero picker/index.js
@@ -14,7 +14,6 @@ console.log('\n');
 function getRandomNumber(arr) {
     // get random index value
     const randomIndex = Math.floor(Math.random() * arr.length);
-
     // get random item
     const item = arr[randomIndex];
     // console.log(item)
