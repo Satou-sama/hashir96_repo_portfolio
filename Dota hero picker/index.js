0e3b4730eb066d7467e5b7a54657e4a374c0404c
Hashir Anis <hashir.work@hotmail.com>
2024-12-25 14:16:56 +0500

    Cleaned up code

--- Dota hero picker/index.js
+++ Dota hero picker/index.js
@@ -6,11 +6,6 @@ import herodata from './herodata.json' with { type: 'json' };
 // Heroes JSON
 // console.log(herodata.result.heroes);
 
-    const heroId = herodata.result.heroes;
-    const heroName = herodata.result.heroes;
-
-console.log('\n');
-
 function getRandomNumber(arr) {
     // get random index value
     const randomIndex = Math.floor(Math.random() * arr.length);
@@ -18,13 +13,11 @@ function getRandomNumber(arr) {
     const item = arr[randomIndex];
     // console.log(item)
     return item;
-
 }
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber(herodata.result.heroes);
 //   console.log(randomNumber);
-  
   selectedHero.textContent = randomNumber.localized_name;
 });
 
